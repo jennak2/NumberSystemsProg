b924162a96872a15503fd1bfd53dc81435915827
jenna.k2 <jkronenberg2@hwemail.com>
2020-01-20 10:03:19 -0800

    trying to commit things from base 10 but it doesnt work

--- js/part1.js
+++ js/part1.js
@@ -14,8 +14,36 @@ function ConversionPart1() {
       }
       outputValue = v;
     }
-
-
+    else
+    {
+      outputValue = "";
+      var value = UnsignedInt;
+    //convert to base
+    while(value != 0)
+    {
+      var nextDigit = value % UnsignedIntBaseTo;
+      if(nextDigit >= 10){
+        if(nextDigit == 10)
+          nextDigit = "A";
+        else if(nextDigit == 11)
+          nextDigit = "B";
+        else if(nextDigit == 12)
+          nextDigit = "C";
+        else if(nextDigit == 13)
+          nextDigit = "D";
+        else if(nextDigit == 14)
+          nextDigit = "E";
+        else if(nextDigit == 15)
+          nextDigit = "F";
+      }
+      else{
+        nextDigit = nextDigit.toString();
+      }
+      outputValue = nextDigit + (outputValue.toString()).charAt(0);
+      value = Math.trunc(value / UnsignedIntBaseTo);
+    }
+    outputValue = parseInt(outputValue);
+}
 
   // Show the output on the screen
   FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, outputValue], 1);
